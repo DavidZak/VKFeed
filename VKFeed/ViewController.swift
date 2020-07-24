42c484a474b4c92f2f17514f937450644ba67505
davidzakharyan <davka126@mail.ru>
2020-07-24 21:08:02 +0400

    init proj

---
+++ VKFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKFeed
+//
+//  Created by David Zakharyan on 24.07.2020.
+//  Copyright © 2020 David Zakharyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
